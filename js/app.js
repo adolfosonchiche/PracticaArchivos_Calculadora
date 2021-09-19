46b6df4fa2e3d837358a179780dda16b998eb39d
adolfosonchiche <adolfoson36@gmail.com>
2021-09-18 18:16:12 -0600

    accion en js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 function calcular(){
     let val1 = document.getElementById("num1").value;
     let val2 = document.getElementById("num2").value;
+    let operacion = document.getElementById("operador").value;
+    console.log(operacion);
     if(val1 === "" || val2 === ""){
      
         alert("tienes que ingresar los 2 numeros para operarlos " );
@@ -9,4 +11,5 @@ function calcular(){
       console.log('hola mundo');
       alert("valor uno = " + val1 + "\n valor dos = " + val2);
     }
+    document.write('<h1>'+ operacion+' </h1>')
 }
